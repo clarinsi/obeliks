9d7fc54707c3b7b0422be17c9da343565776ad14
msinkec <mihael@sinkec.xyz>
2020-09-28 16:08:27 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='obeliks',
+      version='1.0',
+      description='Sentence splitting & tokenization',
+      author='CLARIN.SI',
+      url='https://www.github.com/clarinsi/obeliks',
+      packages=['obeliks'],
+      scripts=['obeliks/obeliks'],
+      package_data={'obeliks': ['res/*.txt']}
+     )
