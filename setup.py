b77dbe8bce2445b1cc8a68fb805a2c8b8a292db6
Luka <krsnik.luka92@gmail.com>
2020-10-01 15:28:56 +0200

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from os import path
 
-setup(name='obeliks',
-      version='1.0',
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(long_description=long_description,
+      long_description_content_type="text/markdown",
+      name='obeliks',
+      version='1.0.1',
       description='Sentence splitting & tokenization',
       author='CLARIN.SI',
       url='https://www.github.com/clarinsi/obeliks',
